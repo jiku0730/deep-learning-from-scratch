dfc5a959bcbdcb374b79d69883be23242883500b
jiku0730 <kei2003730@gmail.com>
2025-11-02 16:37:00 +0900

    matplotlibの使い方わかってきた。1.6.1終了

--- Chapter-1/1.6_Matplotlib.py
+++ Chapter-1/1.6_Matplotlib.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arrange(0, 6, 0,1)
+x = np.arange(0, 6, 0.1)
 y = np.sin(x)
 
 plt.plot(x, y)
-plt.show()
+# plt.show()
+# 仮想環境上のせいか、WSLのせいなのか、GUIに制限が掛かってて表示できない。
+# 代わりに画像を保存することで対応。
+plt.savefig("result.png")
